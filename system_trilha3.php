c01012850f4d083e0814087de28f89b808b230f1
guisilvas <guilherme7soaress@gmail.com>
2023-06-05 19:02:26 -0300

    Change courses name, link to name

--- system_trilha3.php
+++ system_trilha3.php
@@ -94,11 +94,11 @@
                             echo "<div class='courseList'>";
                             if($finish == 1) {
                                 echo "<input type='checkbox' name='curso'  data-curso-id='$idCurso' checked>";
-                                echo "<label for='curso'>" . $curso_link ."</label>";
+                                echo "<a class=nome_curso href=" . $curso_link . " for='curso' target=\"\_blank\"\">" . $curso_nome ."</a>";
 
                             } else {
                                 echo "<input type='checkbox' name='curso' data-curso-id='$idCurso'>";
-                                echo "<label for='curso'>" . $curso_link ."</label>";
+                                echo "<a class=nome_curso href=" . $curso_link . " for='curso' target=\"\_blank\"\">" . $curso_nome ."</a>";
                             }
                             echo "</div>";
                             $contadorCursos++;
