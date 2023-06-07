9b16c8cbd621325197d2dece291949463b4bc7e3
Gabriel Mendonça <gabrielmendonca3827@gmail.com>
2023-06-07 08:40:00 -0300

    Commit update images topbar

--- system.php
+++ system.php
@@ -48,7 +48,7 @@
 <body>
     <header>
         <nav class="navbar"> 
-            <img src="assets/perfil.png" id="perfil">
+            <img src="./assets/perfil.png" id="perfil">
             <div class="bn">
                 <form method="post" action="">
                     <input type="submit" name="sair" value="sair">
